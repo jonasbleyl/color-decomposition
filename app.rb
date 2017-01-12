62bfaa6e65b65d5fefe9faaea96b6fe491eb7e15
Jonas Bleyl <jonas.bleyl@gmail.com>
2017-01-12 08:56:46 +0000

    Create colour space converters

--- app.rb
+++ app.rb
@@ -5,13 +5,25 @@ require 'rmagick'
 
 include Magick
 
-pixels = []
-img = ImageList.new("mushroom.png")
-scanline = img.export_pixels(0, 0, img.columns, 1, "RGB");
+# pixels = []
+# img = ImageList.new("mushroom.png")
+# scanline = img.export_pixels(0, 0, img.columns, 1, "RGB");
+#
+# scanline.each_slice(3) do |pixel|
+#   rgb = { red: (pixel[0]/65535)*255, green: (pixel[1]/65535)*255, blue: (pixel[2]/65535)*255 }
+#   pixels.push(rgb)
+# end
+#
+# p pixels
 
-scanline.each_slice(3) do |pixel|
-  rgb = { red: (pixel[0]/65535)*255, green: (pixel[1]/65535)*255, blue: (pixel[2]/65535)*255 }
-  pixels.push(rgb)
-end
+x, y, z = ColourConverter.rgb_to_xyz(120, 17, 17)
+p "XYZ: #{x} #{y} #{z}"
 
-p pixels
+l, a, b = ColourConverter.xyz_to_lab(x, y, z)
+p "LAB: #{l} #{a} #{b}"
+
+# l, c, h = ColourConverter.lab_to_lch(l, a, b)
+# p "LCH: #{l} #{c} #{h}"
+
+# l1, a1, b1 = ColourConverter.rgb_to_lab(120, 17, 17)
+# l2, a2, b2 = ColourConverter.rgb_to_lab(189, 40, 40)
---
+++ colour_converter.rb
@@ -0,0 +1,64 @@
+require 'matrix'
+
+module ColourConverter
+  def self.rgb_to_xyz(red, green, blue)
+    r = xyz_channel(red / 255.0)
+    g = xyz_channel(green / 255.0)
+    b = xyz_channel(blue / 255.0)
+
+    # sRGB D65 illuminant and 2° observer values
+    s = Matrix[[0.4124, 0.3576, 0.1805],
+               [0.2126, 0.7152, 0.0722],
+               [0.0193, 0.1192, 0.9505]]
+
+    xyz = (s * Matrix[[r], [g], [b]]) * 100
+    [xyz[0, 0], xyz[1, 0], xyz[2, 0]]
+  end
+
+  def self.xyz_to_lab(x, y, z)
+    # CIEXYZ white point values (D65)
+    x = f(x / 95.047)
+    y = f(y / 100.000)
+    z = f(z / 108.883)
+
+    l = (116 * y - 16)
+    a = 500 * (x - y)
+    b = 200 * (y - z)
+    [l.clamp(0, 100), a, b]
+  end
+
+  # def self.lab_to_lch(l, a, b)
+  #   c = Math.sqrt(a**2 + b**2)
+  #   h = Math.atan2(b, a)
+  #
+  #   h = if h > 0
+  #         (h / Math::PI) * 180
+  #       else
+  #         360 - (h.abs / Math::PI) * 180
+  #       end
+  #   [l, c, h]
+  # end
+
+  def self.rgb_to_lab(r, g, b)
+    x, y, z = rgb_to_xyz(r, g, b)
+    xyz_to_lab(x, y, z)
+  end
+
+  private
+
+  def self.xyz_channel(colour)
+    if colour > 0.04045
+      ((colour + 0.055) / 1.055)**2.4
+    else
+      colour / 12.92
+    end
+  end
+
+  def self.f(t)
+    if t > 0.008856
+      t**(1.0 / 3)
+    elsif t <= 0.008856
+      7.787 * t + 16.0 / 116
+    end
+  end
+end
