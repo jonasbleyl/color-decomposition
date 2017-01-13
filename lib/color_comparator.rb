221739d1841cea45621c5e3f6340399eec9435f4
Jonas Bleyl <jonas.bleyl@gmail.com>
2017-01-13 13:28:55 +0000

    Move files to lib folder

--- lib/color_comparator.rb
+++ lib/color_comparator.rb
@@ -8,7 +8,7 @@ module ColorComparator
 
     l_delta = lch2[:l] - lch1[:l]
     c_delta = lch2[:c] - lch1[:c]
-    h_delta = hue_delta(lch1, lch2)
+    h_delta = hue_angle(lch1, lch2)
     o_delta = 30 * Math.exp(-((h_bar - 275) / 25)**2)
 
     t = 1 - 0.17 * Math.cos(radians(h_bar - 30)) +
@@ -20,8 +20,7 @@ module ColorComparator
     sl = 1 + 0.015 * (l_bar - 50)**2 / Math.sqrt(20 + (l_bar - 50)**2)
     sc = 1 + 0.045 * c_bar
     sh = 1 + 0.015 * c_bar * t
-    rt = -Math.sin(radians(2 * o_delta)) * rc
-
+    rt = -Math.sin(radians(o_delta) * 2) * rc
     Math.sqrt((l_delta / sl)**2 + (c_delta / sc)**2 + (h_delta / sh)**2 +
               rt * (c_delta / sc) * (h_delta / sh)).clamp(0, 100)
   end
@@ -34,18 +33,20 @@ module ColorComparator
     c_bar_star = (c1_star + c2_star) / 2
 
     g = 0.5 * (1 - Math.sqrt(c_bar_star**7 / (c_bar_star**7 + 25**7)))
-    a1 = (1 + g) * lab1[:a]
-    a2 = (1 + g) * lab2[:a]
-    c1 = Math.sqrt(a1**2 + lab1[:b]**2)
-    c2 = Math.sqrt(a2**2 + lab2[:b]**2)
-
-    h1 = lab1[:b] != 0 || a1 != 0 ? degrees(Math.atan2(lab1[:b], a1)) : 0
-    h2 = lab2[:b] != 0 || a2 != 0 ? degrees(Math.atan2(lab2[:b], a2)) : 0
-
-    [{ l: lab1[:l], c: c1, h: h1 }, { l: lab2[:l], c: c2, h: h2 }]
+    [{ l: lab1[:l] }.merge(modified_hue(lab1, g)),
+     { l: lab2[:l] }.merge(modified_hue(lab2, g))]
   end
 
-  def self.hue_delta(lch1, lch2)
+  def self.modified_hue(lab, g)
+    a = (1 + g) * lab[:a]
+    c = Math.sqrt(a**2 + lab[:b]**2)
+
+    h = lab[:b] != 0 || a != 0 ? degrees(Math.atan2(lab[:b], a)) : 0
+    h += 360 if h < 0
+    { c: c, h: h }
+  end
+
+  def self.hue_angle(lch1, lch2)
     h_diff = lch2[:h] - lch1[:h]
     h = if lch1[:c] * lch2[:c] == 0
           0
