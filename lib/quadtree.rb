e7b7677f806d67b047904e5878d2093541d71b79
Jonas Bleyl <jonas.bleyl@gmail.com>
2017-02-07 13:28:26 +0000

    Code cleanup for quadtree generation

--- lib/quadtree.rb
+++ lib/quadtree.rb
@@ -12,47 +12,54 @@ class Quadtree
     @nodes.flatten[0]
   end
 
-  def generate
-    tempNodes = []
-    tempRow = []
+  def generate(diff_amount)
     while @nodes.size > 1
-      squares = (nodes.size * nodes[0].size) / 4
-      p "#{nodes.size} #{nodes[0].size}"
-      row = 0
-      col = 0
-
-      squares.times do
-        n1 = @nodes[row][col]
-        n2 = @nodes[row][col + 1]
-        n3 = @nodes[row + 1][col]
-        n4 = @nodes[row + 1][col + 1]
-        if n1.is_leaf? && n2.is_leaf? && n3.is_leaf? && n4.is_leaf?
-          v1 = ColorComparator.ciede2000(n1.lab, n2.lab)
-          v2 = ColorComparator.ciede2000(n3.lab, n4.lab)
-          v3 = ColorComparator.ciede2000(n1.lab, n3.lab)
-          node = merge([n1, n2, n3, n4], (v1 < 1 && v2 < 1 && v3 < 1))
-          tempRow.push(node)
-        else
-          node = merge([n1, n2, n3, n4], false)
-          tempRow.push(node)
-        end
-
-        if col + 2 == @nodes[0].size
-          col = 0
-          row += 2
-          tempNodes.push(tempRow.dup)
-          tempRow.clear
-        else
-          col += 2
-        end
-      end
+      level_nodes = generate_level(diff_amount)
       @nodes.clear
-      @nodes = tempNodes.dup
-      tempNodes.clear
+      @nodes = level_nodes
     end
   end
 
   private
+
+  def generate_level(diff_amount)
+    squares = (@nodes.size * @nodes[0].size) / 4
+    tempNodes = []
+    tempRow = []
+    row = 0
+    col = 0
+
+    squares.times do
+      n1 = @nodes[row][col]
+      n2 = @nodes[row][col + 1]
+      n3 = @nodes[row + 1][col]
+      n4 = @nodes[row + 1][col + 1]
+      if n1.is_leaf? && n2.is_leaf? && n3.is_leaf? && n4.is_leaf?
+        node = merge([n1, n2, n3, n4], similar?(n1, n2, n3, n4, diff_amount))
+        tempRow.push(node)
+      else
+        node = merge([n1, n2, n3, n4], false)
+        tempRow.push(node)
+      end
+
+      if col + 2 == @nodes[0].size
+        col = 0
+        row += 2
+        tempNodes.push(tempRow.dup)
+        tempRow.clear
+      else
+        col += 2
+      end
+    end
+    tempNodes
+  end
+
+  def similar?(n1, n2, n3, n4, amount)
+    v1 = ColorComparator.ciede2000(n1.lab, n2.lab)
+    v2 = ColorComparator.ciede2000(n3.lab, n4.lab)
+    v3 = ColorComparator.ciede2000(n1.lab, n3.lab)
+    v1 < amount && v2 < amount && v3 < amount
+  end
 
   def merge(nodes, as_leaf)
     rect = { left: nodes[0].rect[:left], top: nodes[0].rect[:top],
