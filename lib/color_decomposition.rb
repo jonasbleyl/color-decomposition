75fb47a70c910832244deda12a8629bf1eeb4811
Jonas Bleyl <jonas.bleyl@gmail.com>
2017-02-17 16:34:39 +0000

    Create namespace for quadtree type

--- lib/color_decomposition.rb
+++ lib/color_decomposition.rb
@@ -1,11 +1,11 @@
 require 'color_decomposition/version'
-require 'color_decomposition/quadtree/quadtree'
+require 'color_decomposition/quadtree/quadtree_bottom_up'
 require 'color_decomposition/image/image_reader'
 
 module ColorDecomposition
   def self.quadtree(path, similarity)
     image = ImageReader.new(path)
-    quadtree = Quadtree.new(image.base_image_nodes)
+    quadtree = Quadtree::BottomUp.new(image.base_image_nodes)
     quadtree.generate(similarity)
     quadtree.root
   end
--- test/quadtree_test.rb
+++ test/quadtree_test.rb
@@ -1,17 +1,17 @@
-require 'color_decomposition/quadtree/quadtree'
+require 'color_decomposition/quadtree/quadtree_bottom_up'
 require 'test/unit'
 
 class TestColorComparator < Test::Unit::TestCase
   def test_four_nodes
     rgb = { r: 255, g: 255, b: 255 }
-    n1 = Node.new(rgb, left: 0, top: 0, right: 1, bottom: 1)
-    n2 = Node.new(rgb, left: 1, top: 0, right: 2, bottom: 1)
-    n3 = Node.new(rgb, left: 0, top: 1, right: 1, bottom: 2)
-    n4 = Node.new(rgb, left: 1, top: 1, right: 2, bottom: 2)
-    quadtree = Quadtree.new([[n1, n2], [n3, n4]])
+    n1 = Node.new({ left: 0, top: 0, right: 1, bottom: 1 }, rgb)
+    n2 = Node.new({ left: 1, top: 0, right: 2, bottom: 1 }, rgb)
+    n3 = Node.new({ left: 0, top: 1, right: 1, bottom: 2 }, rgb)
+    n4 = Node.new({ left: 1, top: 1, right: 2, bottom: 2 }, rgb)
+    quadtree = Quadtree::BottomUp.new([[n1, n2], [n3, n4]])
     quadtree.generate(1)
 
     assert(quadtree.root.leaf?)
-    assert_equal(quadtree.root.rect, left: 0, top: 0, right: 2, bottom: 2)
+    assert_equal({ left: 0, top: 0, right: 2, bottom: 2 }, quadtree.root.rect)
   end
 end
