04bd81787cf027c8c9cf6f03c960250265ccc7ce
Jonas Bleyl <jonas.bleyl@gmail.com>
2017-02-28 23:48:52 +0000

    Raise error if similarity value is not between 0 and 100

--- lib/color_decomposition.rb
+++ lib/color_decomposition.rb
@@ -4,6 +4,9 @@ require 'color_decomposition/image/image_reader'
 
 module ColorDecomposition
   def self.quadtree(path, similarity)
+    unless similarity.between?(0, 100)
+      raise ArgumentError, 'Similarity value must be between 0 and 100'
+    end
     image = ImageReader.new(path)
     quadtree = Quadtree.new(image.height, image.width)
     image.add_image_data(quadtree)
